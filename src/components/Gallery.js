a6980a3c67b758dce31b3cb4cd4aa2d7f7fdc30d
Adnan <adnanvandyck@gmail.com>
2024-01-22 05:36:54 -0500

    useEffect and main app functioning correctly

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import GalleryItem from './GalleryItem'
 
-function Gallery() {
+function Gallery(props) {
   return (
-    <div>Gallery</div>
+    <div>
+        <GalleryItem />
+    </div>
   )
 }
 
