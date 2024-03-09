53d02c4a8beeb2140549fbdd6415064946519c01
Adnan <adnanvandyck@gmail.com>
2024-03-09 12:30:57 -0500

    working on merging

--- src/App.js
+++ src/App.js
@@ -1,60 +1,60 @@
-import React, { useState, Suspense, useRef } from 'react'
-import './App.css'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Gallery from './components/Gallery';
-import SearchBar from './components/SearchBar'
-import AlbumView from './components/AlbumView';
-import ArtistView from './components/ArtistView';
-import Spinner from './Spinner';
-import { DataContext } from './context/DataContext';
-import { SearchContext } from './context/SearchContext';
-import { createResource as fetchData } from './helper'
+import React, { useState, Suspense, useRef } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Gallery from "./components/Gallery";
+import SearchBar from "./components/SearchBar";
+import AlbumView from "./components/AlbumView";
+import ArtistView from "./components/ArtistView";
+import Spinner from "./Spinner";
+import { DataContext } from "./context/DataContext";
+import { SearchContext } from "./context/SearchContext";
+import { createResource as fetchData } from "./helper";
 
 function App() {
-
-  let searchInput = useRef('')
-  let [data, setData] = useState(null)
-  let [message, setMessage] = useState('Search for Music!')
+  let searchInput = useRef("");
+  let [data, setData] = useState(null);
+  let [message, setMessage] = useState("Search for Music!");
 
   const handleSearch = (e, term) => {
-    e.preventDefault()
-    setData(fetchData(term, 'main'))
-  }
+    e.preventDefault();
+    setData(fetchData(term, "main"));
+  };
 
   const renderGallery = () => {
-        if(data){
-          return (
-            <Suspense fallback={<Spinner />}>
-            <Gallery data={data}/>
-            </Suspense>
-          )
-        }
-      }
-
+    if (data) {
       return (
-            <div className="App">
-              {message}
-              <Router>
-              <Route exact path={'/'}>
-                  <SearchContext.Provider value={{
-                term: searchInput,
-                handleSearch: handleSearch
-              }}>
+        <Suspense fallback={<Spinner />}>
+          <Gallery data={data} />
+        </Suspense>
+      );
+    }
+  };
+
+  return (
+    <div className="App">
+      {message}
+      <SearchContext.Provider
+        value={{
+          term: searchInput,
+          handleSearch: handleSearch,
+        }}
+      >
+        <DataContext.Provider value={data}>
+          <Routes>
+            <Route exact path={"/"}>
               <SearchBar />
-              </SearchContext.Provider>
-              <DataContext.Provider value={data}>
+
               {renderGallery()}
-              </DataContext.Provider>
-              </Route>
-                  <Route path="/album/:id">
-                    <AlbumView />
-                    </Route>
-                  <Route path="/artist/:id" element={<ArtistView />} />
-               </Router>
-            </div>
-          );
+            </Route>
+            <Route path="/album/:id">
+              <AlbumView />
+            </Route>
+            <Route path="/artist/:id" element={<ArtistView />} />
+          </Routes>
+        </DataContext.Provider>
+      </SearchContext.Provider>
+    </div>
+  );
 }
 
-export default App
-
-
+export default App;
